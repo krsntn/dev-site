e3acf96b6cd9a61b2c94187d5f7e165753c1417e
krsntn <kanahan0110@gmail.com>
2020-02-12 14:49:44 +0800

    update porfolio

--- src/pages/Portfolio/PortfolioPage.js
+++ src/pages/Portfolio/PortfolioPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ScrollToNext from '../../components/ScrollToNext/ScrollToNext';
 import portfolioItems from './portfolio-items.json';
 
-import css from './style.module.css';
+import css from './style.module.scss';
 
 const PortfolioPage = () => {
   return (
@@ -13,37 +13,53 @@ const PortfolioPage = () => {
         <div className={css.portfolio_wrapper}>
           {portfolioItems.map((item, i) => (
             <div className={css.portfolio_item} key={i}>
-              <div className={css.portfolio_item_cover} style={{ background: `url(https://source.unsplash.com/random?sig=${i}/800x453/)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} />
+              <div
+                className={css.portfolio_item_cover}
+                style={{
+                  background: `url(https://source.unsplash.com/random?sig=${i}/800x453/)`,
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                }}
+              />
               <div className={css.portfolio_item_mask}>
                 <div className={css.portfolio_item_links}>
-                  <a target="_blank" rel="noopener noreferrer" href={item.link.href}>{item.link.text}</a>
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href={item.link.href}
+                  >
+                    {item.link.text}
+                  </a>
                 </div>
               </div>
 
               <div className={css.portfolio_item_content}>
-                <div className={css.portfolio_item_title}>
-                  {item.title}
-                </div>
+                <div className={css.portfolio_item_title}>{item.title}</div>
 
                 <div className={css.portfolio_item_desc}>
                   {item.description}
                 </div>
 
                 <div className={css.portfolio_item_icon}>
-                  {
-                    item.icons.map((item, i) => (
-                      <i className={`fab fa-${item} fa-2x`} style={{ fontSize: '1.25rem', margin: '0 5px' }} key={i}></i>
-                    ))
-                  }
+                  {item.icons.map((item, i) => {
+                    const fontawesomeClass = item.slice(0, 3);
+                    const icon = item.slice(4);
+                    return (
+                      <i
+                        className={`${fontawesomeClass} fa-${icon}`}
+                        style={{ fontSize: '1.25rem', margin: '0 5px' }}
+                        key={i}
+                      ></i>
+                    );
+                  })}
                 </div>
               </div>
             </div>
-          ))
-          }
-        </div >
-      </div >
-      <ScrollToNext pageSelector=".landing-page" direction="up" />
-    </div >
+          ))}
+        </div>
+      </div>
+      <ScrollToNext pageSelector='.landing-page' direction='up' />
+    </div>
   );
 };
 
